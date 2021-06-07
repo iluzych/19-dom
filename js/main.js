49333b67fa90a47785af7575b6ea8acaa6697071
iluzych <ieva.daugvilaite@gmail.com>
2021-06-07 20:30:05 +0300

    Uzeigeles pirminis

--- js/main.js
+++ js/main.js
@@ -1,8 +1,26 @@
 const submitDOM = document.querySelector('button');
 const orderDOM = document.querySelector('.order');
+const nameDOM = document.querySelector('#customer');
+const sriubaDOM = document.querySelector('#sriuba');
+const patiekalasDOM = document.querySelector('#patiekalas');
+const desertasDOM = document.querySelector('#desertas');
+const gerimaiDOM = document.querySelectorAll('input[name="gerimas"]');
+
+function arNoriu(DOM) {
+    return DOM.checked ? 'nori' : 'nenori';
+}
+
+function kurisPazymetas(DOMlist) {
+    for (const itemDOM of DOMlist) {
+        if (itemDOM.checked) {
+            return itemDOM.value;
+        }
+    }
+}
 
 submitDOM.addEventListener('click', (e) => {
     e.preventDefault();
-    orderDOM.innerText = 'Uzsakovas vardu [vardas] [nori/nenori] sriubos, [nori/nenori] pagrindinio patiekalo, [nori/nenori] deserto ir [gerimo pavadinimas] yra pasirinktas gerimas.';
+
+    orderDOM.innerText = `Uzsakovas vardu ${nameDOM.value} ${arNoriu(sriubaDOM)} sriubos, ${arNoriu(patiekalasDOM)} pagrindinio patiekalo, ${arNoriu(desertasDOM)} deserto ir ${kurisPazymetas(gerimaiDOM)} yra pasirinktas gerimas.`;
     
 })
